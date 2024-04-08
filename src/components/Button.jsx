153f92610301bea2041297ea3514d050cc6c3144
Bradley Chery <Bradley434@hotmail.com>
2024-04-08 01:56:31 -0400

    adding features to Hero page

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = ({ label, iconURL }) => {
+  return (
+    <button className=''>
+      {label}
+
+      {/* <img /> */}
+    </button>
+  )
+}
+
+export default Button
